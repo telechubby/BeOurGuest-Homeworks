576a3141e2d7134b6b28e7dd8230c8afdd420b66
Martin Trifunov <macotrifunov@gmail.com>
2022-12-24 15:45:00 +0100

    Added Confirm Password & Form Validation
    Frontend:
    -Implemented Confirm Password & Form validation
    -Clear forms on button click
    -Bug fixes

--- Homework 3/client/src/pages/Settings.jsx
+++ Homework 3/client/src/pages/Settings.jsx
@@ -12,9 +12,11 @@ import {
 } from 'mdb-react-ui-kit';
 
 const Settings = () => {
+    //useStates
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     const [nameEdit, unlockNameEdit] = useState(true);
     const [emailEdit, unlockEmailEdit] = useState(true);
     const [passwordEdit, unlockPasswordEdit] = useState(true);
@@ -26,9 +28,21 @@ const Settings = () => {
     let hasNumber = /(.*[0-9].*)/.test(password);
     let hasSpecialChar = /(.*[^a-zA-Z0-9].*)/.test(password);
 
+    //Clear the forms on button click
     const handleSubmit = (e) => {
         e.preventDefault();
     }
+    const handleName = (val) => {
+        setName("");
+    };
+    const handleEmail = (val) => {
+        setEmail("");
+    };
+    const handlePassword = (val) => {
+        setPassword("");
+        setConfirmPassword("");
+    };
+    
       
     return (
         <MDBContainer className='mt-5 mb-5 col-10 col-sm-8 col-md-6 col-lg-5'>
@@ -38,7 +52,8 @@ const Settings = () => {
                     <input className='form-control' placeholder="Name" type='text' disabled={nameEdit} value={name} onChange={(e) => setName(e.target.value)} />
                     <MDBBtn color='dark' onClick={e => { 
                                                         const val = e.target.value;
-                                                        unlockNameEdit(prev => !prev)
+                                                        unlockNameEdit(prev => !prev);
+                                                        handleName();
                                                         }
                                                 }
                     >
@@ -49,7 +64,8 @@ const Settings = () => {
                     <input className='form-control' placeholder="Email" type='email' disabled={emailEdit} value={email} onChange={(e) => setEmail(e.target.value)} />
                     <MDBBtn color='dark' onClick={e => { 
                                                         const val = e.target.value;
-                                                        unlockEmailEdit(prev => !prev)
+                                                        unlockEmailEdit(prev => !prev);
+                                                        handleEmail();
                                                         }
                                                 }
                     >
@@ -60,7 +76,8 @@ const Settings = () => {
                     <input className='form-control' placeholder="Password" type='password' disabled={passwordEdit} value={password} onChange={(e) => setPassword(e.target.value)} />
                     <MDBBtn color='dark' onClick={e => { 
                                                         const val = e.target.value;
-                                                        unlockPasswordEdit(prev => !prev)
+                                                        unlockPasswordEdit(prev => !prev);
+                                                        handlePassword();
                                                         }
                                                 }
                     >
@@ -68,7 +85,7 @@ const Settings = () => {
                     </MDBBtn>
                 </MDBInputGroup>
 
-                {password &&
+                {password && !passwordEdit &&
                     <div className='ms-1' style={{columns: 2}}>
                         <div>
                             {hasEightChar ? (
@@ -138,7 +155,34 @@ const Settings = () => {
                     </div>
                 }
 
-                <MDBBtn color='dark' className='mt-4' block>
+                { !passwordEdit && <input className='form-control' placeholder="Confirm Password" type='password' disabled={passwordEdit} value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} /> }
+
+                { confirmPassword && !passwordEdit &&
+                    <div className='ms-1' style={{columns: 2}}>
+                        <div>
+                            { confirmPassword === password ? (
+                                <span className='text-success'>
+                                    <i className="fa fa-check" aria-hidden="true"></i>
+                                    <small> passwords must match</small>
+                                </span>
+                            ) : (
+                                <span className='text-danger'>
+                                    <i className="fa fa-times" aria-hidden="true"></i>
+                                    <small> passwords must match</small>
+                                </span>
+                            )}
+                        </div>
+                    </div>
+                }
+                <MDBBtn color='dark' className='mt-4' block disabled={ 
+                    (!passwordEdit && (!(confirmPassword == password) ||
+                    (!hasEightChar ||
+                    !hasLowerChar ||
+                    !hasUpperChar ||
+                    !hasNumber ||
+                    !hasSpecialChar))
+                    )
+                    }>
                         <MDBIcon icon='code' fas /> Save Changes
                 </MDBBtn>
             </form>
