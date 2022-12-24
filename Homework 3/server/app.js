b95991a6aba17a20d22017162e8ed1a99f709f86
telechubby <88198476+telechubby@users.noreply.github.com>
2022-12-24 11:51:46 +0100

    Install dependencies on server-side

--- Homework 3/server/app.js
+++ Homework 3/server/app.js
@@ -1,7 +1,17 @@
-const express = require('express');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require("cors");
+require("dotenv").config();
   
 const router = express();
-const PORT = 9000;
+const PORT = process.env.SERVER_PORT;
+
+router.use(cors({ origin: true, credentials: true }));
+router.set('views', path.join(__dirname, 'views'));
+router.set('view engine', 'jade');
   
 const usersRouter=require("./routes/users")
 
