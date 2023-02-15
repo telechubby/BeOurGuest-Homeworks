184bb7d2a2f52b15df171993adfde93c66f8cac4
Ljubomir Kolev <88198476+telechubby@users.noreply.github.com>
2023-02-15 12:56:29 +0100

    Update users.js

--- Homework 4/server/routes/users.js
+++ Homework 4/server/routes/users.js
@@ -62,7 +62,7 @@ router.post('/login',(req,res)=>{
       }
       else{
         const token = jwt.sign({_id: found._id}, process.env.JWT_SECRET_KEY, { expiresIn: "1h"});
-        res.cookie("jwt", token, {expire: new Date() + 9999, httpOnly: false, sameSite:'none',secure:true});
+        res.cookie("jwt", token, {expire: new Date() + 9999, httpOnly: true});
         return res.json({
           message: "Successfully Logged In",
           id: found._id,
